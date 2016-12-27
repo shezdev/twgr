88eff59766a52dd93f14c186edf7891da5232f6b
Shereen <shereenfinney@gmail.com>
2016-12-27 23:39:19 +0000

    Using the -cw command-line flag on a broken version of c2f.rb

---
+++ broken_c2f.rb
@@ -0,0 +1,11 @@
+# “$ ruby -cw c2f.rb
+# The -cw command-line flag is shorthand for two flags: -c and -w. The -c flag means check for syntax errors. The -w flag activates a higher level of warning: Ruby will fuss at you if you’ve done things that are legal Ruby but are questionable on grounds other than syntax.”
+
+# If syntax ok you will see the message Syntax OK
+
+
+celcius = 100
+fahrenheit = (celcius * 9 / 5 + 31
+puts "The result is: "
+puts fahrenheit
+puts "."
